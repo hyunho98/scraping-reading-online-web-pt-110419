d5176884efd65ec2ac7020f9667de33e266fe193
Alex Lee <hyunho98@gmail.com>
2020-01-03 00:39:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,4 @@ doc = Nokogiri::HTML(html)
 
 courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
-p courses[0].name
+p courses[0].attributes
